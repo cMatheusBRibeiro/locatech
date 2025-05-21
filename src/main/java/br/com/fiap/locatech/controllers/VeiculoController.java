3650054c3a963dae88ca3d383a88504c6f03f85e
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-05-21 19:50:54 -0300

    feat(VeiculoController): Add findAllVeiculos and findVeiculoById routes

--- src/main/java/br/com/fiap/locatech/controllers/VeiculoController.java
+++ src/main/java/br/com/fiap/locatech/controllers/VeiculoController.java
@@ -1,16 +1,45 @@
 package br.com.fiap.locatech.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.locatech.entities.Veiculo;
+import br.com.fiap.locatech.services.VeiculoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/veiculos")
 public class VeiculoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(VeiculoController.class);
+
+    private final VeiculoService veiculoService;
+
+    public VeiculoController(VeiculoService veiculoService) {
+        this.veiculoService = veiculoService;
+    }
+
     @GetMapping
-    public String getVeiculos() {
-        return "Hello veiculos";
+    public ResponseEntity<List<Veiculo>> findAllVeiculos(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
+    ) {
+        logger.info("/veiculos");
+        var veiculos = veiculoService.findAllVeiculos(page, size);
+        return ResponseEntity.ok(veiculos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Veiculo>> findVeiculoById(
+            @PathVariable("id") Long id
+    ) {
+        logger.info("/veiculos/" + id);
+        var veiculo = veiculoService.findVeiculoById(id);
+        return ResponseEntity.ok(veiculo);
     }
 
 }
