1ec82da5b8e9a734e79e841ef5c81560cb78bb8f
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-05-21 20:22:52 -0300

    feat(VeiculoController): Add saveVeiculo, updateVeiculo and deleteVeiculoById

--- src/main/java/br/com/fiap/locatech/controllers/VeiculoController.java
+++ src/main/java/br/com/fiap/locatech/controllers/VeiculoController.java
@@ -4,6 +4,7 @@ import br.com.fiap.locatech.entities.Veiculo;
 import br.com.fiap.locatech.services.VeiculoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +29,7 @@ public class VeiculoController {
             @RequestParam("page") int page,
             @RequestParam("size") int size
     ) {
-        logger.info("/veiculos");
+        logger.info("GET -> /veiculos");
         var veiculos = veiculoService.findAllVeiculos(page, size);
         return ResponseEntity.ok(veiculos);
     }
@@ -37,9 +38,37 @@ public class VeiculoController {
     public ResponseEntity<Optional<Veiculo>> findVeiculoById(
             @PathVariable("id") Long id
     ) {
-        logger.info("/veiculos/" + id);
+        logger.info("GET -> /veiculos/{}", id);
         var veiculo = veiculoService.findVeiculoById(id);
         return ResponseEntity.ok(veiculo);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveVeiculo(
+            @RequestBody Veiculo veiculo
+    ) {
+        logger.info("POST -> /veiculos");
+        veiculoService.save(veiculo);
+        return ResponseEntity.status(HttpStatus.CREATED.value()).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateVeiculo(
+            @RequestParam Long id,
+            @RequestBody Veiculo veiculo
+    ) {
+        logger.info("PUT -> /veiculos/{}", id);
+        veiculoService.update(veiculo, id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT.value()).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVeiculoById(
+            @RequestParam Long id
+    ) {
+        logger.info("DELETE -> /veiculos/{}", id);
+        veiculoService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT.value()).build();
     }
 
 }
